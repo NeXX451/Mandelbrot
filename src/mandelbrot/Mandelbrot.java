1f29c94edcd48cd4cd5ac5c8f9aea3edbc2f177d
Katja Hahn <nebbich21@web.de>
2013-07-12 21:24:45 +0200

    Further improvements to coloring

--- src/mandelbrot/Mandelbrot.java
+++ src/mandelbrot/Mandelbrot.java
@@ -5,20 +5,20 @@ import java.awt.image.BufferedImage;
 
 public class Mandelbrot {
 
-	private static final int MAX_IT = 300;
 	private static final float ESCAPE_RADIUS = 2f;
+	private static final int MAX_OF_MAX_IT = 1000;
 	
+	private int maxIt = 100;
 	private final int width;
 	private final int height;
-
-	private double reC;
-	private double imC;
 
 	private double reStart = -2.15;
 	private double imStart = 1.50;
 	private double reEnd = 0.85;
 	private double step;
 
+	private double reC;
+	private double imC;
 	private double reZ;
 	private double imZ;
 
@@ -48,14 +48,14 @@ public class Mandelbrot {
 	}
 
 	private void initColors() {
-		int numOfColors = MAX_IT;
-		double rgb = 0;
+		int numOfColors = maxIt;
 		colors = new Color[numOfColors];
 		float hue = 0;
-		float step = 0.01f;
+		float step = 0.003f;
 		for (int i = 0; i < colors.length; i++) {
+			step = (float) (1 / (Math.log(i + 2) * 100));
 			hue += step;
-			colors[i] = new Color(Color.HSBtoRGB(hue, 1f, 0.5f));
+			colors[i] = new Color(Color.HSBtoRGB(hue, 1f, 1f));
 		}
 	}
 
@@ -67,25 +67,24 @@ public class Mandelbrot {
 
 				reC = reStart + step * x;
 				imC = imStart - step * y;
+				
 				reZ = 0;
 				imZ = 0;
 				calculateF();
 
-				if (itCounter >= MAX_IT) {
+				if (itCounter >= maxIt) {
 					image.setRGB(x, y, Color.black.getRGB());
 				} else {
 					modulus = absoluteValue(reZ, imZ);
 					double mu = itCounter + 1 - Math.log(Math.log(modulus))
 							/ Math.log(ESCAPE_RADIUS);
-					mu = mu / MAX_IT * colors.length;
+					mu = mu / maxIt * colors.length;
 					image.setRGB(x, y, getColor(mu).getRGB());
 				}
 			}
 		}
 		return image;
 	}
-
-	boolean first = true;
 
 	/**
 	 * Thanks to the following site that helped me smoothing the colors:
@@ -106,8 +105,6 @@ public class Mandelbrot {
 				.getGreen() * t2);
 		int blue = (int) (colors[clr1].getBlue() * t1 + colors[clr2].getBlue()
 				* t2);
-		if(first) System.out.println("r: " + red + "g: " + green + "b: " + blue);
-		first = false;
 		return new Color(red, green, blue);
 	}
 
@@ -116,7 +113,7 @@ public class Mandelbrot {
 		do {
 			iterationStep();
 			modulus = absoluteValue(reZ, imZ);
-		} while (modulus <= ESCAPE_RADIUS && itCounter < MAX_IT);
+		} while (modulus <= ESCAPE_RADIUS && itCounter < maxIt);
 
 		iterationStep();
 		iterationStep();
@@ -139,9 +136,21 @@ public class Mandelbrot {
 		this.imStart = imStart;
 		this.reEnd = reEnd;
 		this.step = (reEnd - reStart) / width;
+		double scale = 1 / (reEnd - reStart);
+		System.out.println("scale: " + scale);
+		calculateMaxIt(scale);
+		if(maxIt > MAX_OF_MAX_IT) maxIt = MAX_OF_MAX_IT;
+		System.out.println("MAX_IT: " + maxIt);
+		initColors();
 		System.out.println("new values: reStart " + reStart + ", imStart "
 				+ imStart + ", reEnd " + reEnd + ", step " + step);
 		return createImage();
+	}
+
+	//got the maxIt calculation from: http://math.stackexchange.com/questions/16970/a-way-to-determine-the-ideal-number-of-maximum-iterations-for-an-arbitrary-zoom
+	private void calculateMaxIt(double scale) {
+		this.maxIt = (int) (Math.sqrt(Math.abs(2 * Math.sqrt(Math.abs(1 - Math
+				.sqrt(5 * scale))))) * 66.5);
 	}
 
 	public BufferedImage getImage() {
